40cff3da90e4d9cd206c1520437807926bf45607
singhayush10 <singhayush8638@gmail.com>
2023-08-20 05:35:43 +0530

    Update routes.cpp: Add calculation logic

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,41 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
+
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON");
+    }
+
+    double first = input["first"].d();
+    double second = input["second"].d();
+    double result = first + second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
 }
+
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON");
+    }
+
+    double first = input["first"].d();
+    double second = input["second"].d();
+    double result = first - second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
 }
